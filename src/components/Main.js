32b62041a341266c954abea892f29dc0912793ad
abucheer <arahmanbucheeri@gmail.com>
2024-02-14 12:54:28 +0000

    added headline, edit in styles

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,25 @@
+import headlineImg from '../images/restauranfood.jpg'
+
 function Main() {
     return (
-        <main></main>
+        <main>
+            <div className="headline">
+                <div className="row">
+                    <section>
+                        <h2>Little Lemon</h2>
+                        <sub>Chicago</sub>
+                        <p>Come visit our nice new restaurant by reserving your favorite table with the click of a button below.</p>
+                    </section>
+                    <section>
+                        <img
+                            src={headlineImg}
+                            alt='headline-image-showing-restaurant-food'
+                            width='150'
+                        />
+                    </section>
+                </div>
+            </div>
+        </main>
     );
 }
 
