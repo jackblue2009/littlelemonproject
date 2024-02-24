bd22ca12d37c522199ea99f29f2fde725af17096
abucheer <arahmanbucheeri@gmail.com>
2024-02-24 21:56:46 +0000

    added and fixed links and routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Homepage from './components/Homepage';
 import './App.css';
+import BookingPage from './components/BookingPage';
 
 function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<MainComp />} />
+        <Route path='/booking' element={<BookingComp />} />
+      </Routes>
+    </Router>
+  );
+}
+
+function MainComp() {
   return (
     <>
       <Header />
       <Homepage />
+      <Footer />
+    </>
+  );
+}
+
+function BookingComp() {
+  return (
+    <>
+      <Header />
+      <BookingPage />
       <Footer />
     </>
   );
--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -3,7 +3,16 @@ import headlineImg from '../images/restauranfood.jpg'
 import Specials from './Specials';
 import CustomerSay from './CustomerSay';
 import Chicago from './Chicago';
+import BookingPage from './BookingPage';
+import { Link } from 'react-router-dom';
+
 function Homepage() {
+    // const openPopup = () => {
+    //     const popupWindow = window.open('/booking', 'popupWndow', 'width=800,height=600');
+    //     if (popupWindow) {
+    //         popupWindow.focus();
+    //     }
+    // }
     return (
         <main>
             <div className="headline">
@@ -13,7 +22,9 @@ function Homepage() {
                         <sub>Chicago</sub>
                         <p>Come visit our nice new restaurant by reserving your favorite table with the click of a button below.</p>
                         <br/>
-                        <button>Book A Table</button>
+                        <Link to='/booking'>Book A Table</Link>
+                        {/* <button onClick={openPopup}>Book A Table</button> */}
+                        {/* <a href='/booking'>Book A Table</a> */}
                     </section>
                     <section>
                         <img
