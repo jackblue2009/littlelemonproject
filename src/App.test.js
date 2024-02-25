9cd5c2a5b51c83b03ced65b1809122f6a56e0037
abucheer <arahmanbucheeri@gmail.com>
2024-02-25 23:42:41 +0000

    added unit testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,26 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, fireEvent, screen } from "@testing-library/react";
+import App from "./App";
+import BookingPage from "./components/BookingPage";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// test("Adds one", () => {
+//   // render the App component
+//   render(<App />);
+
+//   // save the heading in a variable
+//   const heading = screen.getByTestId("currentNumber");
+
+//   // save the button in a variable
+//   const btn = screen.getByTestId("addOne");
+
+//   // click the btn
+//   fireEvent.click(btn);
+
+//   // test assumption
+//   expect(heading).toHaveTextContent("2");
+// });
+
+test('Renders the BookingForm heading', () => {
+  render(<BookingPage />);
+  const headingElement = screen.getByText("Book A Table");
+  expect(headingElement).toBeInTheDocument();
 });
